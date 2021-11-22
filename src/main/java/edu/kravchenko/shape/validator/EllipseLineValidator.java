25d8e984c657141c873a464ba65033f83c485d65
Polina Kravchenko <lotushiems@gmail.com>
2021-11-22 16:41:09 +0300

    add line validator

---
+++ src/main/java/edu/kravchenko/shape/validator/EllipseLineValidator.java
@@ -0,0 +1,12 @@
+package edu.kravchenko.shape.validator;
+
+public class EllipseLineValidator {
+    private static final String VALID_ELLIPSE_LINE_REGEXP = "([-]?\\d+\\.\\d+\\s+){3}([-]?\\d+\\.\\d+)";
+
+    private EllipseLineValidator() {
+    }
+
+    public static boolean isValidLine(String line) {
+        return line.matches(VALID_ELLIPSE_LINE_REGEXP);
+    }
+}
