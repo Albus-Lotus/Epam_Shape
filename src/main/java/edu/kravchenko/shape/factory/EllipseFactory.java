82dff838b0eecdc7512d4ff0769449e0b5ec3144
Polina Kravchenko <lotushiems@gmail.com>
2021-11-22 14:53:56 +0300

    add factory

---
+++ src/main/java/edu/kravchenko/shape/factory/EllipseFactory.java
@@ -0,0 +1,35 @@
+package edu.kravchenko.shape.factory;
+
+import edu.kravchenko.shape.entity.Ellipse;
+import edu.kravchenko.shape.entity.Point;
+import edu.kravchenko.shape.exception.EllipseException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+public class EllipseFactory {
+    private static final EllipseFactory instance = new EllipseFactory();
+    private static final int VALUE_COUNT = 4;
+    private static final Logger logger = LogManager.getLogger();
+
+    private EllipseFactory() {
+    }
+
+    public Ellipse createEllipse(List<Double> coordinates) throws EllipseException {
+        if (coordinates.size() != VALUE_COUNT) {
+            logger.log(Level.ERROR, "Invalid arguments count");
+            throw new EllipseException("Invalid arguments count");
+        }
+        Point firstPoint = new Point(coordinates.get(0), coordinates.get(1));
+        Point secondPoint = new Point(coordinates.get(2), coordinates.get(3));
+        Ellipse ellipse = new Ellipse(firstPoint, secondPoint);
+        logger.log(Level.INFO, "Ellipse was created");
+        return ellipse;
+    }
+
+    public static EllipseFactory getInstance() {
+        return instance;
+    }
+}
