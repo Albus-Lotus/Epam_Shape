b0a113417051dd060fb1641f7d3d034e60aa7872
Polina Kravchenko <lotushiems@gmail.com>
2021-11-22 13:22:49 +0300

    add parameters validator

---
+++ src/main/java/com/edu/kravchenko/shape/validator/EllipseParametersValidator.java
@@ -0,0 +1,17 @@
+package com.edu.kravchenko.shape.validator;
+
+import com.edu.kravchenko.shape.entity.Point;
+
+public class EllipseParametersValidator {
+
+    public EllipseParametersValidator() {
+    }
+
+    public static boolean areValidParameters(Point firstPoint, Point secondPoint) {
+        if (firstPoint == null || secondPoint == null) {
+            return false;
+        }
+        return firstPoint.getX() != secondPoint.getX()
+                && firstPoint.getY() != secondPoint.getY();
+    }
+}
